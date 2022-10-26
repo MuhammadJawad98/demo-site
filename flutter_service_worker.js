bb06ccc5b0a67eed0ebd39fc4b5cc637bfd1731c
MJawad8 <m.jawad@vfairs.com>
2022-10-26 15:22:19 +0500

    noch color change test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3be58becd52c79274433ae1ee29cc769",
-"index.html": "3de5bc4355a41fad9fded086fe8f5559",
-"/": "3de5bc4355a41fad9fded086fe8f5559",
-"main.dart.js": "60a65b0ef1fffc7e2165de9a69ffea11",
+"index.html": "2c043bdca2169f74e0bad8b9fc80febe",
+"/": "2c043bdca2169f74e0bad8b9fc80febe",
+"main.dart.js": "e7a881a7b563e0ae60d1670e37ad8d18",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
