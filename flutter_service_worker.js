6478540d0aa28d396334116d8eaa2473d3c1b8f5
MJawad8 <m.jawad@vfairs.com>
2022-10-26 14:23:56 +0500

    primary color change

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3be58becd52c79274433ae1ee29cc769",
-"index.html": "80c85fd00e2e7a2644314e93d99046ce",
-"/": "80c85fd00e2e7a2644314e93d99046ce",
-"main.dart.js": "b8a19ec6d279fd02c6974a985b82d435",
+"index.html": "ccd30683349c1b000b6f43fb8cf70bfb",
+"/": "ccd30683349c1b000b6f43fb8cf70bfb",
+"main.dart.js": "15554c731955af493373983f819c5fd0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
